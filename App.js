7800f358c086f45df2759ee3bf634ed06cf8335d
Barath97 <barath.kj97@gmail.com>
2024-01-16 16:32:22 +0530

    Functional Companents

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading = React.createElement("h1",{ id:"heading"},"Hello world from React!");
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//JSX Babel(transpiled before it reaches the JS engine) - PARCEL-Babel
+
+//React Element
+const Title = () => <h1 id="jsx">Namaste react using React!</h1>;
+
+//functional components
+const FunctionalComponents = () =>(
+    <div id="container">
+        <Title />
+        <h1 id="heading">Namaste React from functional components</h1>
+    </div>
+);
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponents/>);
